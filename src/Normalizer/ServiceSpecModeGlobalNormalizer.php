3546eec873d41802239081791394855de4c26bde
Flavio Heleno <flaviohbatista@gmail.com>
2020-12-13 02:11:47 -0300

    Updated all dependencies to latest version

---
+++ src/Normalizer/ServiceSpecModeGlobalNormalizer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Docker\API\Normalizer;
+
+use Docker\API\Runtime\Normalizer\CheckArray;
+use Jane\JsonSchemaRuntime\Reference;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ServiceSpecModeGlobalNormalizer implements DenormalizerInterface, NormalizerInterface, DenormalizerAwareInterface, NormalizerAwareInterface
+{
+    use CheckArray;
+    use DenormalizerAwareTrait;
+    use NormalizerAwareTrait;
+
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return 'Docker\\API\\Model\\ServiceSpecModeGlobal' === $type;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return \is_object($data) && 'Docker\\API\\Model\\ServiceSpecModeGlobal' === \get_class($data);
+    }
+
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        if (isset($data['$ref'])) {
+            return new Reference($data['$ref'], $context['document-origin']);
+        }
+        if (isset($data['$recursiveRef'])) {
+            return new Reference($data['$recursiveRef'], $context['document-origin']);
+        }
+        $object = new \Docker\API\Model\ServiceSpecModeGlobal();
+        if (null === $data || false === \is_array($data)) {
+            return $object;
+        }
+
+        return $object;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $data = [];
+
+        return $data;
+    }
+}
